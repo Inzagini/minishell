6b2f0b74c0cadaa153015021ec7f0703b2e52721
Quan Anh Nguyen <quannguy@c3a5c2.42berlin.de>
2025-01-07 15:42:18 +0100

    Initial commit

--- minishell.h
+++ minishell.h
@@ -6,13 +6,25 @@
 /*   By: quannguy <quannguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:24:37 by quannguy          #+#    #+#             */
-/*   Updated: 2025/01/07 15:24:42 by quannguy         ###   ########.fr       */
+/*   Updated: 2025/01/07 15:40:56 by quannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHEL_H
+# define MINISHELL_H
 
+#include <stdio.h>
 
+# define PIPE
+# define IN_FILE
+# define OUT_FILE
+
+typedef struct s_token_structure
+{
+	int		type;
+	char	*content;
+	t_token	*previous;
+	t_token	*next;
+}	t_token;
 
 #endif
