bab46bdff8ee1cbbd2f9714a7626ec20149438c9
Quan Anh Nguyen <quannguy@c3a5c2.42berlin.de>
2025-01-07 17:03:31 +0100

    Initial commt

--- main.c
+++ main.c
@@ -1,6 +1,1 @@
 #include "minishell.h"
-
-int	main(void)
-{
-	printf("%s\n", readline("Test prompt:"));
-}
--- minishell.h
+++ minishell.h
@@ -6,18 +6,24 @@
 /*   By: quannguy <quannguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:24:37 by quannguy          #+#    #+#             */
-/*   Updated: 2025/01/07 15:40:56 by quannguy         ###   ########.fr       */
+/*   Updated: 2025/01/07 16:01:10 by quannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
+//define type of the tokens
+# define CMD
 # define PIPE
 # define IN_FILE
 # define OUT_FILE
+
+typedef struct s_token_structure	t_token;
 
 typedef struct s_token_structure
 {
