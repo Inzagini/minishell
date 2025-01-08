94d966ec69f1c3bd3ab528670b42cf36129f8567
Paul Buchter <pbuchter@c3a8c2.42berlin.de>
2025-01-08 11:38:06 +0100

    [minishell.h] added command struct

--- parser.c
+++ parser.c
@@ -1,41 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbuchter <pbuchter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/07 15:24:37 by quannguy          #+#    #+#             */
-/*   Updated: 2025/01/08 11:33:13 by pbuchter         ###   ########.fr       */
+/*   Created: 2025/01/08 11:33:33 by pbuchter          #+#    #+#             */
+/*   Updated: 2025/01/08 11:35:21 by pbuchter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-#include <stdio.h>
-
-# define PIPE
-# define IN_FILE
-# define OUT_FILE
-
-typedef struct s_token_structure
+t_command	*parser(t_token *)
 {
-	int		type;
-	char	*content;
-	t_token	*previous;
-	t_token	*next;
-}	t_token;
-
-typedef struct s_command
-{
-	char		*name;
-	char		**arguments;
-	t_command	*next;
-	int			*redir_in;
-	int			*redir_out;
-	char		*redir_file;
-}	t_command;
 
 
-#endif
+
+}
