f61a78f98a73fb65a29260387b0eb9e34acd7aec
Quan Anh Nguyen <quannguy@c3a8c1.42berlin.de>
2025-01-08 11:36:39 +0100

    [minishell.h] - token types

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: quannguy <quannguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 15:24:37 by quannguy          #+#    #+#             */
-/*   Updated: 2025/01/07 16:01:10 by quannguy         ###   ########.fr       */
+/*   Updated: 2025/01/08 11:35:06 by quannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,33 @@
 # include <readline/history.h>
 
 //define type of the tokens
-# define CMD
-# define PIPE
-# define IN_FILE
-# define OUT_FILE
-
 typedef struct s_token_structure	t_token;
+
+typedef enum e_token_types
+{
+	CMD,
+	ARG,
+	PIPE,
+	RD_OUT,
+	RD_IN,
+	RD_APP,
+	RD_ININ,
+	SQUOTE,
+	DQUOTE,
+	LPAREN,
+	RPAREN,
+	ENV_VAR,
+	NEW_LINE,
+	INVAL
+}	t_token_type;
 
 typedef struct s_token_structure
 {
-	int		type;
-	char	*content;
-	t_token	*previous;
-	t_token	*next;
+	t_token_type	type;
+	char			*content;
+	t_token			*previous;
+	t_token			*next;
 }	t_token;
+
 
 #endif
