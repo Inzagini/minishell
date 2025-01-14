2e8d6d32cbef161824cfff58cb894231a75f7874
Quan Anh Nguyen <quannguy@c3a8c1.42berlin.de>
2025-01-14 14:26:06 +0100

    echo done

---
+++ build_in_echo.c
@@ -0,0 +1,43 @@
+#include "minishell.h"
+
+int	contain_only(char *str, char c);
+
+void	ft_echo(t_command *cmd)
+{
+	int	index;
+	int	n_flag;
+
+	index = 0;
+	n_flag = 0;
+	while (cmd->args[++index])
+	{
+		if (cmd->args[index][0] != '-')
+			break ;
+		if (contain_only(cmd->args[index], 'n'))
+			break ;
+		else
+			n_flag = 1;
+	}
+	while (cmd->args[index])
+	{
+		printf("%s", cmd->args[index++]);
+		if (cmd->args[index])
+			printf(" ");
+	}
+	if (!n_flag)
+		printf("\n");
+}
+
+int	contain_only(char *str, char c)
+{
+	int	index;
+
+	index = 0;
+	while (str[++index])
+	{
+		if (str[index] != c)
+			return (1);
+	}
+	return (0);
+}
+
