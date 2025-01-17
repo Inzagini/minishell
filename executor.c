42785d051b8c5b42dea6a5444d9b3f97f49d840e
Quan Anh Nguyen <quannguy@c3a8c1.42berlin.de>
2025-01-17 14:24:30 +0100

    add execute_built_in back

--- executor.c
+++ executor.c
@@ -6,7 +6,7 @@ void	executor(t_command *cmd_list, t_env *env)
 
 	len = lst_len(cmd_list);
 	if (cmd_list->builtin_flag && lst_len(cmd_list) < 2) // we need to call commands that are not part of a "|""
-		call_builtin(cmd_list, env);
+		execute_build_in(cmd_list, env);
 	else
 	{
 		call_pipe_line(cmd_list, env);
