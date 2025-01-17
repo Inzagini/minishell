3bef276374badbafff60988c1282d0e667e5c3a0
pbuchter <pbuchter@student.42berlin.de>
2025-01-17 18:37:49 +0100

    fixed expander

--- expander_args_split.c
+++ expander_args_split.c
@@ -27,77 +27,31 @@ int	init_split(t_split *split, char **env, t_token **arg_list, t_token *arg)
 	return (0);
 }
 
+
 void	update_arg_ids(t_split *split, t_token *arg)
 {
+	int	base_id;
+	int	i;
+	int	new_id;
+
+	base_id = arg->arg_group_id;
+	new_id = base_id + split->count_token - split->merge_prev - split->merge_next + 1;
 	split->update = split->next;
+	printf("Content %s %d\n", split->update->content, new_id);
 	while (split->update)
 	{
-		if (split->update->next
-			&& split->update->arg_group_id == split->update->next->arg_group_id)
-			split->update->arg_group_id = arg->arg_group_id
-				+ split->count_token - split->merge_next - split->merge_prev;
-		else
-		{
-			split->update->arg_group_id = arg->arg_group_id
-				+ split->count_token - split->merge_next - split->merge_prev;
-			split->count_token++;
-		}
+		split->update->arg_group_id = new_id;
+		printf("Content %s %d\n", split->update->content, new_id);
+		if (split->update->next && split->update->next->arg_group_id != split->update->arg_group_id)
+			new_id = new_id + 1;
 		split->update = split->update->next;
 	}
 }
 
-// int	split_argument(t_token **arg_list, t_token *arg, char **env, int arg_id)
-// {
-// 	t_split	split;
-// 	int		i;
-
-// 	init_split(&split, env, arg_list, arg);
-// 	if (split.count_token == 1)
-// 	{
-// 		arg->content = ft_strdup(split.split_tokens[0]);
-// 		return (free_split(split.split_tokens), 0);
-// 	}
-// 	i = -1;
-// 	while (split.split_tokens[++i])
-// 	{
-// 		split.new_token = create_token_split(split.split_tokens[i], (arg_id)++);
-// 		if (!split.new_token)
-// 		{
-// 			free_split(split.split_tokens);
-// 			return (1);
-// 		}
-// 		if (i == 0 && split.prev)
-// 		{
-// 			merge_tokens(split.prev, split.new_token);
-// 			split.last_added = split.prev;
-// 		}
-// 		else if (!split.split_tokens[i + 1] && split.next)
-// 		{
-// 			if (split.next->arg_group_id == arg->arg_group_id)
-// 				merge_tokens(split.new_token, split.next);
-// 			else
-// 			{
-// 				split.last_added->next = split.new_token;
-// 				split.new_token->next = split.next;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			if (split.last_added)
-// 				insert_token_after(arg_list, split.last_added, split.new_token);
-// 			else
-// 				insert_token_after(arg_list, arg, split.new_token);
-// 			split.last_added = split.new_token;
-// 		}
-// 	}
-// 	free_split(split.split_tokens);
-// 	remove_token(arg_list, arg);
-// 	return (0);
-// }
-
 int	expand_arguments_noquote(t_env *env, t_command *cmd_list)
 {
 	t_token	*arg;
+	t_token *test;
 
 	while (cmd_list)
 	{
@@ -143,7 +97,10 @@ int	handle_token_merge(t_split *split, t_token *arg, t_token **arg_list, int i)
 	else if (!split->split_tokens[i + 1] && split->next)
 	{
 		if (split->next->arg_group_id == arg->arg_group_id)
+		{
 			merge_tokens(split->new_token, split->next);
+			split->last_added->next = split->new_token;
+		}
 		else
 		{
 			split->last_added->next = split->new_token;
@@ -175,6 +132,7 @@ int	split_argument(t_token **arg_list, t_token *arg, char **env, int arg_id)
 		handle_token_merge(&split, arg, arg_list, i);
 	}
 	free_split(split.split_tokens);
+
 	remove_token(arg_list, arg);
 	return (0);
 }
--- usefull.md
+++ usefull.md
@@ -2,3 +2,5 @@ cd ~/Work/minishell
 ls | cat -e > test
 cd ~/Work
 ls
+
+echo eabc$VAR"$VAR"$VAR'abc'
