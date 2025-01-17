30734196038094a6d40f56943c7a5ba3535deaed
pbuchter <pbuchter@student.42berlin.de>
2025-01-17 13:07:02 +0100

    found bug in executor, see comment

--- executor.c
+++ executor.c
@@ -5,7 +5,7 @@ void	executor(t_command *cmd_list, t_env *env)
 	int	len;
 
 	len = lst_len(cmd_list);
-	if (cmd_list->builtin_flag && lst_len(cmd_list) < 2)
+	if (cmd_list->builtin_flag && lst_len(cmd_list) < 2) // we need to call commands that are not part of a "|""
 		call_builtin(cmd_list, env);
 	else
 	{
