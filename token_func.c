ed7a12502cf213a5759e6dd77c0e8fc90448d22a
Quan Anh Nguyen <quannguy@c3a8c1.42berlin.de>
2025-01-20 13:25:13 +0100

    restructure

---
+++ token_func.c
@@ -0,0 +1,35 @@
+#include "minishell.h"
+
+t_token	*create_token(char *content, t_token_type token_type)
+{
+	t_token	*new_token;
+
+	new_token = (t_token *)malloc(sizeof(t_token));
+	if (!new_token)
+		return (NULL);
+	new_token->type = token_type;
+	new_token->content = content;
+	new_token->quote_identifier = 0;
+	new_token->arg_group_id = 0;
+	new_token->previous = NULL;
+	new_token->next = NULL;
+	return (new_token);
+}
+
+void	append_token_lst(t_token **head, t_token *new_token)
+{
+	t_token	*tmp;
+
+	if (!head || !new_token)
+		return ;
+	if (!*head)
+	{
+		*head = new_token;
+		return ;
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new_token;
+	new_token->previous = tmp;
+}
--- tokenizer.c
+++ tokenizer.c
@@ -2,6 +2,7 @@
 
 int	meta_char(char *str, t_data *data, t_token **head);
 static void	token_init(t_data *data);
+int	is_symbol_char(char c);
 
 int	tokenizer(char *input_str, t_token **head)
 {
@@ -12,11 +13,7 @@ int	tokenizer(char *input_str, t_token **head)
 	{
 		if (!meta_char(input_str, &data, head))
 			continue ;
-		else if (input_str[data.index + 1] == ' '
-			|| input_str[data.index + 1] == 0
-			|| input_str[data.index + 1] == '\n'
-			|| input_str[data.index + 1] == '\''
-			|| input_str[data.index + 1] == '"')
+		else if (is_symbol_char(input_str[data.index + 1]))
 		{
 			if (data.cmd_flag == 0 && !data.rd_flag)
 				cmd_handle(input_str, &data, head);
@@ -58,36 +55,8 @@ int	meta_char(char *input_str, t_data *data, t_token **head)
 	return (0);
 }
 
-t_token	*create_token(char *content, t_token_type token_type)
+int	is_symbol_char(char c)
 {
-	t_token	*new_token;
-
-	new_token = (t_token *)malloc(sizeof(t_token));
-	if (!new_token)
-		return (NULL);
-	new_token->type = token_type;
-	new_token->content = content;
-	new_token->quote_identifier = 0;
-	new_token->arg_group_id = 0;
-	new_token->previous = NULL;
-	new_token->next = NULL;
-	return (new_token);
-}
-
-void	append_token_lst(t_token **head, t_token *new_token)
-{
-	t_token	*tmp;
-
-	if (!head || !new_token)
-		return ;
-	if (!*head)
-	{
-		*head = new_token;
-		return ;
-	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new_token;
-	new_token->previous = tmp;
+	return (c == ' ' || c == 0 || c == '\n'
+		|| c == '\'' || c == '"');
 }
