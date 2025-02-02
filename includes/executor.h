a1b4a7928786ae255f438827c6ab613aba3902bf
David Nguyen <toggo1212@gmail.com>
2025-02-02 16:05:47 +0100

    split funtion to fit norm

--- includes/executor.h
+++ includes/executor.h
@@ -15,12 +15,11 @@ typedef struct s_executor_data
 
 void	executor_init(t_exdat *data);
 void	executor(t_command *cmd_list, t_env *env);
-int		call_pipe_line(t_command **cmd_lst, t_env *env);
+int		call_pipe_line(t_command **cmd_lst, t_env *env, t_exdat *data);
 void	execute_build_in(t_command *cmd, t_env *env);
 
-void	call_execve(t_command *data, t_env *env);
-// int		pre_handle(t_command *cmd, t_exdat *data, t_env *env);
 void	invoke_builtin(t_command *cmd, t_env *env);
+void	call_execve(t_command *data, t_env *env);
 
 //build_in functions
 void	call_builtin(t_command *cmd, t_env *env);
