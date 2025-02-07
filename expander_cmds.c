a064a11d7bf541b1e844b8bc3e7188c042ee49a6
pbuchter <pbuchter@student.42berlin.de>
2025-02-07 16:17:22 +0100

    fixed command expansion

--- expander_cmds.c
+++ expander_cmds.c
@@ -6,7 +6,7 @@
 /*   By: pbuchter <pbuchter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:43:35 by pbuchter          #+#    #+#             */
-/*   Updated: 2025/02/04 11:43:36 by pbuchter         ###   ########.fr       */
+/*   Updated: 2025/02/07 16:15:26 by pbuchter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,13 +62,21 @@ char	*get_cmd(char **paths, char *cmd)
 void	expand_commands(t_env *env, t_command *cmd_list)
 {
 	char	*command;
+	char	*cmd_paths;
+	char	*full_path;
 
+	full_path = find_path(env->env);
+	if (!full_path)
+		return ;
+	cmd_paths = ft_split(full_path, ':');
+	if (!cmd_paths)
+		return ;
 	while (cmd_list)
 	{
 		if (cmd_list->builtin_flag == 0 && cmd_list->args[0]
 			&& cmd_list->args[0][0])
 		{
-			command = get_cmd(env->cmd_paths, cmd_list->args[0]);
+			command = get_cmd(cmd_paths, cmd_list->args[0]);
 			if (command)
 			{
 				free (cmd_list->args[0]);
@@ -77,6 +85,7 @@ void	expand_commands(t_env *env, t_command *cmd_list)
 		}
 		cmd_list = cmd_list->next;
 	}
+	free_split(cmd_paths);
 }
 
 void	expand_commands_post(t_env *env, t_command *cmd_list)
